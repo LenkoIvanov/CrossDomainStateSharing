dd60148481dfc2a32cfe3fa302d62954d3016d31
Lenko Ivanov <lenko.ivanov117@gmail.com>
2026-01-13 20:15:41 +0200

    feat: populate the product grid with dummy products

--- client-approach/src/app/page/ProductGrid/ProductCard/ProductCard.tsx
+++ client-approach/src/app/page/ProductGrid/ProductCard/ProductCard.tsx
@@ -1,5 +1,32 @@
-const ProductCard = () => {
-  return <div></div>;
+import { Product } from "@/app/types/Product";
+import styles from "./ProductCard.module.scss";
+import Button from "@/app/common/Button/Button";
+
+interface ProductCardProps {
+  product: Product;
+}
+
+const ProductCard = (props: ProductCardProps) => {
+  const { product } = props;
+  const { name, price, imageUrl, altText } = product;
+
+  return (
+    <div className={styles.productCard}>
+      <div className={styles.imageContainer}>
+        {/* eslint-disable-next-line @next/next/no-img-element */}
+        <img
+          className={styles.productImage}
+          src={imageUrl ?? ""}
+          alt={altText}
+        />
+      </div>
+      <div className={styles.productInfo}>
+        <p className={styles.textContainer}>{name}</p>
+        <p className={`${styles.textContainer} ${styles.price}`}>{price}€</p>
+        <Button type="primary">Add to cart</Button>
+      </div>
+    </div>
+  );
 };
 
 export default ProductCard;
--- client-approach/src/app/page/ProductGrid/ProductGrid.tsx
+++ client-approach/src/app/page/ProductGrid/ProductGrid.tsx
@@ -1,5 +1,15 @@
+import { products } from "@/app/resources/products";
+import ProductCard from "./ProductCard/ProductCard";
+import styles from "./ProductGrid.module.scss";
+
 const ProductGrid = () => {
-  return <section>This is the product grid</section>;
+  return (
+    <section className={styles.productGrid}>
+      {products.map((product) => (
+        <ProductCard key={product.id} product={product} />
+      ))}
+    </section>
+  );
 };
 
 export default ProductGrid;
