e63ba02c59d37f2e63b000201964d91b8e027106
Lenko Ivanov <lenko.ivanov117@gmail.com>
2026-01-13 20:31:02 +0200

    refactor: refactor header styles

--- client-approach/src/app/layout/header/Header.tsx
+++ client-approach/src/app/layout/header/Header.tsx
@@ -6,14 +6,18 @@ const Header = () => {
   return (
     <header className={styles.header}>
       <h1>Client sub-domain</h1>
-      <div className={styles.linkContainer}>
-        <LinkButton href="#" textContent="Home" />
-        <LinkButton href="#" textContent="About us" />
-        <LinkButton href="#" textContent="Contact us" />
-      </div>
       <div className={styles.elementsContainer}>
-        <Button type="primary">Register / Log in</Button>
-        <Button type="secondary">Cart</Button>
+        <div className={styles.linkContainer}>
+          <LinkButton href="#" textContent="Home" />
+          <LinkButton href="#" textContent="About us" />
+          <LinkButton href="#" textContent="Contact us" />
+        </div>
+        <div className={styles.buttonContainer}>
+          <Button type="primary">Register / Log in</Button>
+          <div className={styles.cartContainer}>
+            <Button type="secondary">Cart</Button>
+          </div>
+        </div>
       </div>
     </header>
   );
