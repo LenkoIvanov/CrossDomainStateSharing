c99d54f3b9c34da20f6bd75e2d6e4aeba5acfea5
Lenko Ivanov <lenko.ivanov117@gmail.com>
2026-02-01 11:52:32 +0200

    sync domains via iframes WIP

--- client-approach/src/app/common/SyncDomains.tsx
+++ client-approach/src/app/common/SyncDomains.tsx
@@ -1,9 +1,10 @@
 import { useRef } from "react";
 
 interface SyncDomainsProps {
-  targetDomain:
-    | "https://sub1.clientapp.vercel.com"
-    | "https://sub2.clientapp.vercel.com";
+  // targetDomain:
+  //   | "https://sub1.clientapp.vercel.com"
+  //   | "https://sub2.clientapp.vercel.com";
+  targetDomain: "http://iframe1.local.test" | "http://iframe2.local.test";
 }
 
 export const SyncDomains = ({ targetDomain }: SyncDomainsProps) => {
--- client-approach/src/utils/syncDomains.ts
+++ client-approach/src/utils/syncDomains.ts
@@ -2,8 +2,13 @@ export const domainOne = "https://sub1.clientapp.vercel.com";
 
 export const domainTwo = "https://sub2.clientapp.vercel.com";
 
+export const localOne = "http://iframe1.local.test:3000";
+
+export const localTwo = "http://iframe2.local.test:3001";
+
 export const isDomainOne = (currentSubDomain: string) => {
-  if (currentSubDomain === "sub1") return true;
+  // "sub1"
+  if (currentSubDomain === "iframe1") return true;
 
   return false;
 };
