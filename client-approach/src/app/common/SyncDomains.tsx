0e0a6b08bb88a188a4d9977c21711397063308c5
Lenko Ivanov <lenko.ivanov117@gmail.com>
2026-02-01 22:49:29 +0200

    feat: Add content-security-policy head & handle sender/receiver cases in SyncDomains

--- client-approach/src/app/common/SyncDomains.tsx
+++ client-approach/src/app/common/SyncDomains.tsx
@@ -1,48 +1,72 @@
 import { fetchCartItemsFromLocalStorage } from "@/utils/localStorage";
 import {
+  IframeHandshakeEventType,
   IframePayloadEventKey,
   dispatchUpdateEventType,
 } from "@/utils/syncDomains";
-import { useEffect, useRef } from "react";
+import { useEffect, useEffectEvent, useRef, useState } from "react";
 
 export const SyncDomains = () => {
   const targetDomain = process.env.NEXT_PUBLIC_TARGET_DOMAIN;
+  const [isIframeReady, setIsIframeReady] = useState(false);
   const iframeRef = useRef<HTMLIFrameElement>(null);
+  const isInsideIframe =
+    typeof window !== "undefined" && window.self !== window.top;
 
   const handleIncomingMessage = (ev: MessageEvent) => {
     if (ev.origin !== targetDomain) return;
-    if (ev.data?.key === IframePayloadEventKey) return;
 
-    console.log("Received event from sub-domain: ", ev);
+    if (ev.data?.key === IframePayloadEventKey) {
+      console.log(
+        `Successfully received from ${ev.origin} with cartItems ${ev.data.value}`,
+      );
+    }
   };
 
-  const handlePostMessage = (targetDomain: string) => {
+  const handleHandshake = (ev: MessageEvent) => {
+    if (ev.origin !== targetDomain) return;
+
+    if (ev.data.type === IframeHandshakeEventType) {
+      console.log(`Received handshake from ${ev.origin}`);
+      setIsIframeReady(true);
+    }
+  };
+
+  const handlePostMessage = useEffectEvent(() => {
+    if (!isIframeReady || !targetDomain) return;
+
     const cartItems = fetchCartItemsFromLocalStorage();
     const iframePayload = {
       key: IframePayloadEventKey,
       value: cartItems,
     };
     iframeRef.current?.contentWindow?.postMessage(iframePayload, targetDomain);
-  };
+  });
 
   useEffect(() => {
     if (!targetDomain) return;
 
-    window.addEventListener("message", (ev) => handleIncomingMessage(ev));
-    window.addEventListener(dispatchUpdateEventType, () =>
-      handlePostMessage(targetDomain)
-    );
+    if (isInsideIframe) {
+      window.parent.postMessage({ type: "SYNC_READY" }, targetDomain);
 
-    return () => {
-      window.removeEventListener("message", handleIncomingMessage);
-      window.removeEventListener(dispatchUpdateEventType, () =>
-        handlePostMessage(targetDomain)
-      );
-    };
+      window.addEventListener("message", handleIncomingMessage);
 
+      return () => {
+        window.removeEventListener("message", handleIncomingMessage);
+      };
+    } else {
+      window.addEventListener("message", handleHandshake);
+      window.addEventListener(dispatchUpdateEventType, handlePostMessage);
+
+      return () => {
+        window.removeEventListener("message", handleHandshake);
+        window.removeEventListener(dispatchUpdateEventType, handlePostMessage);
+      };
+    }
     // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [targetDomain]);
+  }, [isInsideIframe, isIframeReady, targetDomain]);
 
+  if (isInsideIframe) return null;
   return (
     <iframe ref={iframeRef} src={targetDomain} style={{ display: "none" }} />
   );
