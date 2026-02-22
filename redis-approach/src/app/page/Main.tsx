d8a5f68ac2ddaba95bacd41346a7c3cd6518ef39
Lenko Ivanov <lenko.ivanov117@gmail.com>
2026-02-22 16:13:35 +0200

    feat: establish a connection with a dummy request

--- redis-approach/src/app/page/Main.tsx
+++ redis-approach/src/app/page/Main.tsx
@@ -1,10 +1,30 @@
 "use client";
 
+import { useEffect } from "react";
 import styles from "./Main.module.scss";
 import ProductGrid from "./ProductGrid/ProductGrid";
 import SideNavigation from "./SideNavigation/SideNavigation";
 
 const Main = () => {
+  useEffect(() => {
+    fetch("http://localhost:3001/cartInfo", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        id: "dsdsdfsdjkfhsdjkhds",
+        cartData: [
+          {
+            id: "wdfjsdkf",
+            name: "Default",
+            qty: 2,
+          },
+        ],
+      }),
+    });
+  }, []);
+
   return (
     <section className={styles.mainContainer}>
       <SideNavigation />
